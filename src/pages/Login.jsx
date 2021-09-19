6bb3b376c6e0191114d90d8207686c4e63bbd514
Hussain Saifuddin <hussainquetta53@gmail.com>
2021-09-19 23:48:48 +0500

    UI changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,6 +6,8 @@ import { isEmpty, isLoaded, useFirebase } from "react-redux-firebase";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import Loader from "react-loader-spinner";
+import { useDispatch } from "react-redux";
+import ThemeAction from "../redux/actions/ThemeAction";
 
 const LogInSchema = Yup.object().shape({
   email: Yup.string().email().required("Email is required"),
@@ -13,17 +15,27 @@ const LogInSchema = Yup.object().shape({
 });
 
 const ERROR = {
-    color: "darkred",
-    fontSize: 12,
-    marginTop: -25,
-    marginLeft: 50,
-    marginBottom: 25,
+  color: "darkred",
+  fontSize: 12,
+  marginTop: -25,
+  marginLeft: 50,
+  marginBottom: 25,
 };
 
 const Login = (props) => {
   const { login, logout } = useFirebase();
   const { auth, profile } = useSelector((state) => state.firebase);
   const history = useHistory();
+
+  const themeReducer = useSelector((state) => state.ThemeReducer);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const themeClass = localStorage.getItem("themeMode", "theme-mode-light");
+    const colorClass = localStorage.getItem("colorMode", "theme-mode-light");
+    dispatch(ThemeAction.setMode(themeClass));
+    dispatch(ThemeAction.setColor(colorClass));
+  }, [dispatch]);
 
   useEffect(() => {
     if (isLoaded(auth) && !isEmpty(auth)) {
@@ -45,51 +57,55 @@ const Login = (props) => {
   }, [profile.isAdmin]);
 
   return (
-    <>
-      <div className="main">
-        <p className="sign" align="center">
-          Sign in
-        </p>
-        <Formik
-            initialValues={{email: '', password: ''}}
+    <div className={`layout ${themeReducer.mode} ${themeReducer.color}`}>
+      <div className="main_body">
+        <div className="main">
+          <p className="sign" align="center">
+            Sign in
+          </p>
+          <Formik
+            initialValues={{ email: "", password: "" }}
             validationSchema={LogInSchema}
-            onSubmit={(values, {setSubmitting, setFieldError}) => {
-                login({
-                    email: values.email,
-                    password: values.password,
-                }).then(() => {
-                    setSubmitting(false);
-                }, reason => {
-                    setSubmitting(false)
-                    setFieldError('firebase', reason.message)
-                });
+            onSubmit={(values, { setSubmitting, setFieldError }) => {
+              login({
+                email: values.email,
+                password: values.password,
+              }).then(
+                () => {
+                  setSubmitting(false);
+                },
+                (reason) => {
+                  setSubmitting(false);
+                  setFieldError("firebase", reason.message);
+                }
+              );
             }}
-        >
-          {({
-            values,
-            errors,
-            touched,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            isSubmitting,
-            /* and other goodies */
-          }) => (
-            <form className="form1">
-              <input
-                type="email"
-                name="email"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.email}
-                placeholder="Enter Your Email..."
-                className="un "
-              />
-              <br />
-              {errors.email && touched.email && (
-                <p style={ERROR}>{errors.email}</p>
-              )}
-              <input
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting,
+              /* and other goodies */
+            }) => (
+              <form className="form1">
+                <input
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.email}
+                  placeholder="Enter Your Email..."
+                  className="un "
+                />
+                <br />
+                {errors.email && touched.email && (
+                  <p style={ERROR}>{errors.email}</p>
+                )}
+                <input
                   name="password"
                   type="password"
                   onChange={handleChange}
@@ -97,40 +113,31 @@ const Login = (props) => {
                   value={values.password}
                   className="pass"
                   placeholder="Enter Your Password..."
-              />
-                <br/>
+                />
+                <br />
                 {/*{JSON.stringify(values,null,2)}*/}
                 {errors.password && touched.password && (
-                    <p style={ERROR}>{errors.password}</p>
+                  <p style={ERROR}>{errors.password}</p>
                 )}
-                {errors.firebase && (
-                    <p style={ERROR}>{errors.firebase}</p>
-                )}
-                {/* <a
-                className="submit"
-                type="submit"
-                disabled={isSubmitting}
-                align="center"
-              >
-                Sign in
-              </a> */}
+                {errors.firebase && <p style={ERROR}>{errors.firebase}</p>}
                 <button
-                    onClick={handleSubmit}
-                type="submit"
-                className="submit"
-                disabled={isSubmitting}
-              >
-                {isSubmitting ? (
-                  <Loader type="Puff" color="white" height={12} width={40} />
-                ) : (
-                  <p>Submit</p>
-                )}
-              </button>
-            </form>
-          )}
-        </Formik>
+                  onClick={handleSubmit}
+                  type="submit"
+                  className="submit"
+                  disabled={isSubmitting}
+                >
+                  {isSubmitting ? (
+                    <Loader type="Puff" color="white" height={12} width={40} />
+                  ) : (
+                    <p>Submit</p>
+                  )}
+                </button>
+              </form>
+            )}
+          </Formik>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
