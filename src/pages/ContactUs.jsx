f468d31251e2d9f5263b8a84a0375257cd3f1ad4
Ahsan Iqbal <axan.iqbal1234@gmail.com>
2022-02-15 19:12:45 +0500

    some update

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,73 +1,73 @@
 import React from 'react';
 import Table from '../components/table/Table';
-import {populate, useFirestoreConnect} from 'react-redux-firebase';
-import {useSelector} from 'react-redux';
+import { populate, useFirestoreConnect } from 'react-redux-firebase';
+import { useSelector } from 'react-redux';
 import _ from 'lodash';
 import '../components/table/table.css';
-import {ModalProvider} from 'react-simple-hook-modal';
+import { ModalProvider } from 'react-simple-hook-modal';
 import 'react-simple-hook-modal/dist/styles.css';
-import ContactUsTable from "../components/contactUsTable/ContactUsTable";
+import ContactUsTable from '../components/contactUsTable/ContactUsTable';
 
 const topCustomers = {
-    head: ['ID', 'User Name', 'User Email', ''],
+  head: ['ID', 'User Name', 'User Email', ''],
 };
 
 const renderCusomerHead = (item, index) => <th key={index}>{item}</th>;
 
 const RenderCusomerBody = (item, index) => {
-    return (
-        <>
-            <tr key={index}>
-                <td>{index + 1}</td>
-                <td>{item.name}</td>
-                <td>{item.address}</td>
-                <td>
-                    <button
-                        className="buttonStyle"
-                        onClick={() => {
-                            console.log(item.requests);
-                        }}
-                    >
-                        View
-                    </button>
-                </td>
-            </tr>
-        </>
-    );
+  return (
+    <>
+      <tr key={index}>
+        <td>{index + 1}</td>
+        <td>{item.name}</td>
+        <td>{item.address}</td>
+        <td>
+          <button
+            className="buttonStyle"
+            onClick={() => {
+              console.log(item.requests);
+            }}
+          >
+            View
+          </button>
+        </td>
+      </tr>
+    </>
+  );
 };
 
 const ContactUS = () => {
-    const firestore = useSelector(state => state.firestore);
-    useFirestoreConnect([
-        {
-            collection: 'contactForm',
-        },
-    ]);
-    const requestData = firestore.ordered.contactForm;
-    console.log(requestData);
-    return (
-        <ModalProvider>
-            <div>
-                <h2 className="page-header">Contact Us Requests</h2>
-                <div className="row">
-                    <div className="col-12">
-                        <div className="card">
-                            <div className="card__body">
-                                <ContactUsTable
-                                    headData={topCustomers.head}
-                                    renderHead={(item, index) => renderCusomerHead(item, index)}
-                                    bodyData={requestData}
-                                    edit={false}
-                                    contactUS={true}
-                                    renderBody={(item, index) => RenderCusomerBody(item, index)}
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  const firestore = useSelector(state => state.firestore);
+  useFirestoreConnect([
+    {
+      collection: 'contactForm',
+    },
+  ]);
+  const requestData = firestore.ordered.contactForm;
+  console.log(requestData);
+  return (
+    <ModalProvider>
+      <div>
+        <h2 className="page-header">Contact Us Requests</h2>
+        <div className="row">
+          <div className="col-12">
+            <div className="card">
+              <div className="card__body">
+                <ContactUsTable
+                  headData={topCustomers.head}
+                  renderHead={(item, index) => renderCusomerHead(item, index)}
+                  bodyData={requestData}
+                  edit={false}
+                  contactUS={true}
+                  renderBody={(item, index) => RenderCusomerBody(item, index)}
+                />
+              </div>
             </div>
-        </ModalProvider>
-    );
+          </div>
+        </div>
+      </div>
+    </ModalProvider>
+  );
 };
 
 export default ContactUS;
--- src/pages/Terms&Condition.js
+++ src/pages/Terms&Condition.js
@@ -1,41 +1,42 @@
 import React from 'react';
-import './login.css'
+import './login.css';
 
 function TermsCondition(props) {
-    return (
-        <div className="termsPage">
-            <h1>Namaz Timings</h1>
-            <div className="termsBox">
-                <h1>Terms & Condition</h1>
-                <p>
-                    NamazTimings.pk application aims to helps Muslims in locating nearby Masajid and their Namaz timings
-                    so
-                    that one could reach on time and catch the jamat. The objective is to help the travellers in praying
-                    their namaz on time.
+  return (
+    <div className="termsPage">
+      <h1>Namaz Timings</h1>
+      <div className="termsBox">
+        <h1>Terms & Condition</h1>
+        <p>
+          NamazTimings.pk application aims to helps Muslims in locating nearby
+          Masajid and their Namaz timings so that one could reach on time and
+          catch the jamat. The objective is to help the travellers in praying
+          their namaz on time. Usage of this application is subject to the
+          following terms and conditions. If anyone person doesn’t agree with
+          the terms, (s)he should uninstall this application and refrain using
+          it.
+        </p>
+        <h3>Data Accuracy </h3>
+        <p>
+          Data of this application is updated by the volunteers or the admin of
+          each Masjid. Whilst the utmost care is taken to ensure the accuracy of
+          the content/information provided in the application, the creators of
+          this application do not guarantee the accuracy of any details provided
+          in the application nor they are responsible for any time and/or wealth
+          loss or any personal grievances whatsoever.
+        </p>
 
-                    Usage of this application is subject to the following terms and conditions. If anyone person doesn’t
-                    agree with the terms, (s)he should uninstall this application and refrain using it.
-                </p>
-                <h3>Data Accuracy </h3>
-                <p>Data of this application is updated by the volunteers or the admin of each Masjid. Whilst the utmost
-                    care is taken to ensure the accuracy of the content/information provided in the application, the
-                    creators of
-                    this application do not guarantee the accuracy of any details provided in the application nor they
-                    are
-                    responsible for any time and/or wealth loss or any personal grievances whatsoever.
-                </p>
-
-                <h3>Privacy</h3>
-                <p>The data collected in the form of user credentials are kept under complete privacy. None of the
-                    information
-                    will be shared with any third party for any purpose. In case of any data leak due to the system
-                    hack/theft
-                    or any other external factor, the creators of this application are not responsible for any damage
-                    whatsoever.
-                </p>
-            </div>
-        </div>
-    );
+        <h3>Privacy</h3>
+        <p>
+          The data collected in the form of user credentials are kept under
+          complete privacy. None of the information will be shared with any
+          third party for any purpose. In case of any data leak due to the
+          system hack/theft or any other external factor, the creators of this
+          application are not responsible for any damage whatsoever.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default TermsCondition;
